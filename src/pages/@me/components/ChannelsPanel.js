4f2c7ef3966f121276fca191af566d9912d094d7
Snowy <snowydragonmc@gmail.com>
2024-03-01 09:20:58 +0300

    Backup upload

--- src/pages/@me/components/ChannelsPanel.js
+++ src/pages/@me/components/ChannelsPanel.js
@@ -1,9 +1,20 @@
 import React from "react";
+import {FiSettings} from "react-icons/fi";
 
 const ChannelsPanel = () => {
     return (
         <div className="db-panel db-l">
-            <div className="db-header">Head</div>
+            <div className="db-header left-head">
+                <div className="profile">
+                    <div className="db-profile-pic"/>
+                    <div className="db-username">Snowy
+                        <div className="db-tag">#0000</div>
+                    </div>
+                </div>
+                <div className="tools">
+                    <div className="tool-icon"><FiSettings/></div>
+                </div>
+            </div>
             <div className="db-content">Content</div>
         </div>
     );
--- src/pages/@me/components/Footer.js
+++ src/pages/@me/components/Footer.js
@@ -1,32 +1,48 @@
 import React from "react";
+import {Overlay, OverlayTrigger, Tooltip} from "react-bootstrap";
 
 export const Footer = () => {
-  const [display, setDisplay] = React.useState("none");
+  const [display, setDisplay] = React.useState(false);
+  const friendsBtn = React.useRef(null);
   return (
     <div className="db-b-bar">
-      <div
-        className="friends-toggle"
-        onClick={() => {
-          setDisplay(display === "none" ? "block" : "none");
-        }}
-      />
-      <div className="servers">
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-        <div className="server"></div>
-      </div>
-      <div className="servers">
-        <div className="server"></div>
-        <div className="server"></div>
-      </div>
-      <div className="friendsPanel" style={{ display: display }}>
-        <div className="down-triangle"></div>
-      </div>
+        <div
+            className="friends-toggle"
+            ref={friendsBtn}
+            onClick={() => {setDisplay(!display)}}
+        />
+        <div className="servers">
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-1">Snowys</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-2">Something</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-3">IDK</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-4">well yeah</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-5">hehehe</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+            <OverlayTrigger placement="top" overlay={<Tooltip id="server-6">kkkkk</Tooltip>}>
+                <div className="server"></div>
+            </OverlayTrigger>
+
+        </div>
+        <div className="servers">
+            <div className="server"/>
+            <div className="server"/>
+        </div>
+            <Overlay target={friendsBtn.current} show={display} placement="top">
+                {({ placement, arrowProps, show: _show, popper, ...props }) => (
+                    <div className="friendsPanel">
+                        Simple tooltip
+                    </div>
+                )}
+            </Overlay>
     </div>
   );
 };
